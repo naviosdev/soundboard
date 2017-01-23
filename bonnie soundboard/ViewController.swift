82df3c952ade7074a3eb5a1ef812909f430084ec
naviosdev <navdevios@writeme.com>
2017-01-23 23:37:35 +0000

    REFACTORRR
    clean up of code, various comments added, new ‘playSound’ function
    added

--- bonnie soundboard/ViewController.swift
+++ bonnie soundboard/ViewController.swift
@@ -16,32 +16,25 @@ var audioIsplaying = Bool()
 class ViewController: UIViewController,UIScrollViewDelegate  {
 
     
-    
-    
+    //properties
     @IBOutlet var pageCotrol: UIPageControl!
-    
     @IBOutlet var myScrollView: UIScrollView!
-    
     @IBOutlet var myView: UIView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        //setting up the scroll view
         myScrollView.contentSize = CGSize(width: 960, height: 100)
         myScrollView.delegate = self
         myScrollView.isPagingEnabled = true
-       
-        
-        
-        
-        
     }
 
     @IBAction func pageAction(_ sender: Any) {
-      
 
-        
+        //when user taps pageControl update scroll view bounds
         switch pageCotrol.currentPage {
         case 0:
             myScrollView.bounds.origin.x = 0
@@ -54,33 +47,11 @@ class ViewController: UIViewController,UIScrollViewDelegate  {
         }
     }
 
-    
-
-    
-    
-    
     func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
         
         
-        
-        /*
-        if pageCotrol.currentPage == 2{
-            pageCotrol.currentPage =  pageCotrol.currentPage - 2
-        } else {
-            
-            pageCotrol.currentPage += 1
-        }*/
-        
         print(myScrollView.bounds)
-        
-        
-        
-        
-        /*
-        if myScrollView.bounds.width == 320 {
-            pageCotrol.currentPage = 2
-        }*/
-        
+        //update page control
         switch myScrollView.bounds.origin.x {
         case 0:
             pageCotrol.currentPage = 0
@@ -91,211 +62,46 @@ class ViewController: UIViewController,UIScrollViewDelegate  {
         default:
             pageCotrol.currentPage = 0
         }
-        
-        
-        
     }
-    
-    
-    
     
     
 
     
     
+    //MARK: Button Actions
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
-    
-    @IBAction func action1(_ sender: Any) {
-        
-        if audioIsplaying == true {
-            
-            audioIsplaying = false
-            
-            audioPlayer.stop()
-        } else {
-            
-            audioIsplaying = true
-            let newSound = URL(fileURLWithPath: Bundle.main.path(forResource: "2", ofType: "mp3")!)
-            
-            do {
-                audioPlayer = try AVAudioPlayer(contentsOf: newSound)
-            }
-            catch {
-                print("error l")
-            }
-            
-            audioPlayer.play()
-        }
-        
-    }
-    
-    
-   
-    
-    @IBAction func action2(_ sender: Any) {
-        
-        let path = Bundle.main.path(forResource: "Sequence 01", ofType:"mp3")!
-        let url = URL(fileURLWithPath: path)
-        
-        do {
-            let sound = try AVAudioPlayer(contentsOf: url)
-            audioPlayer = sound
-            sound.play()
-        } catch {
-            // couldn't load file :(
-            print("error l")
-        }
-        
-        
-    }
-    
-    
-    @IBOutlet var button3: UIButton!
-    
-    @IBAction func action3(_ sender: Any) {
-        
-        button3.backgroundColor = UIColor.red
-        let path = Bundle.main.path(forResource: "01 Nikes", ofType:"mp3")!
-        let url = URL(fileURLWithPath: path)
-        
-        do {
-            let sound = try AVAudioPlayer(contentsOf: url)
-            audioPlayer = sound
-            sound.play()
-        } catch {
-            // couldn't load file :(
-            print("error l")
-        }
-
-    }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    //call bonnie
     
     @IBAction func call1(_ sender: Any) {
-        let path = Bundle.main.path(forResource: "call1", ofType:"mp3")!
-        let url = URL(fileURLWithPath: path)
-        
-        do {
-            let sound = try AVAudioPlayer(contentsOf: url)
-            audioPlayer = sound
-            sound.play()
-        } catch {
-            // couldn't load file :(
-            print("error l")
-        }
+        playSound(soundFile: "call1")
     }
     
     @IBAction func call2(_ sender: Any) {
-        let path = Bundle.main.path(forResource: "call2", ofType:"mp3")!
-        let url = URL(fileURLWithPath: path)
-        
-        do {
-            let sound = try AVAudioPlayer(contentsOf: url)
-            audioPlayer = sound
-            sound.play()
-        } catch {
-            // couldn't load file :(
-            print("error l")
-        }
+        playSound(soundFile: "call2")
     }
     
     @IBAction func call3(_ sender: Any) {
-        let path = Bundle.main.path(forResource: "call3", ofType:"mp3")!
-        let url = URL(fileURLWithPath: path)
-        
-        do {
-            let sound = try AVAudioPlayer(contentsOf: url)
-            audioPlayer = sound
-            sound.play()
-        } catch {
-            // couldn't load file :(
-            print("error l")
-        }
+        playSound(soundFile: "call3")
     }
     
     @IBAction func call4(_ sender: Any) {
-        let path = Bundle.main.path(forResource: "call4", ofType:"mp3")!
-        let url = URL(fileURLWithPath: path)
-        
-        do {
-            let sound = try AVAudioPlayer(contentsOf: url)
-            audioPlayer = sound
-            sound.play()
-        } catch {
-            // couldn't load file :(
-            print("error l")
-        }
+        playSound(soundFile: "call4")
     }
     
     @IBAction func call5(_ sender: Any) {
-        let path = Bundle.main.path(forResource: "call5", ofType:"mp3")!
-        let url = URL(fileURLWithPath: path)
-        
-        do {
-            let sound = try AVAudioPlayer(contentsOf: url)
-            audioPlayer = sound
-            sound.play()
-        } catch {
-            // couldn't load file :(
-            print("error l")
-        }
+        playSound(soundFile: "call5")
     }
     
     @IBAction func call6(_ sender: Any) {
-        let path = Bundle.main.path(forResource: "call6", ofType:"mp3")!
-        let url = URL(fileURLWithPath: path)
-        
-        do {
-            let sound = try AVAudioPlayer(contentsOf: url)
-            audioPlayer = sound
-            sound.play()
-        } catch {
-            // couldn't load file :(
-            print("error l")
-        }
+        playSound(soundFile: "call6")
     }
     
     @IBAction func call7(_ sender: Any) {
-        let path = Bundle.main.path(forResource: "call7", ofType:"mp3")!
-        let url = URL(fileURLWithPath: path)
-        
-        do {
-            let sound = try AVAudioPlayer(contentsOf: url)
-            audioPlayer = sound
-            sound.play()
-        } catch {
-            // couldn't load file :(
-            print("error l")
-        }
+        playSound(soundFile: "call7")
     }
     
     @IBAction func call8(_ sender: Any) {
-        let path = Bundle.main.path(forResource: "Sequence 01", ofType:"mp3")!
-        let url = URL(fileURLWithPath: path)
-        
-        do {
-            let sound = try AVAudioPlayer(contentsOf: url)
-            audioPlayer = sound
-            sound.play()
-        } catch {
-            // couldn't load file :(
-            print("error l")
-        }
+        playSound(soundFile: "call8")
     }
     
     
@@ -305,116 +111,34 @@ class ViewController: UIViewController,UIScrollViewDelegate  {
     //discipline bonnie
     
     @IBAction func d1(_ sender: Any) {
-        let path = Bundle.main.path(forResource: "d1", ofType:"mp3")!
-        let url = URL(fileURLWithPath: path)
-        
-        do {
-            let sound = try AVAudioPlayer(contentsOf: url)
-            audioPlayer = sound
-            sound.play()
-        } catch {
-            // couldn't load file :(
-            print("error l")
-        }
+        playSound(soundFile: "d1")
     }
     
     @IBAction func d2(_ sender: Any) {
-        let path = Bundle.main.path(forResource: "d2", ofType:"mp3")!
-        let url = URL(fileURLWithPath: path)
-        
-        do {
-            let sound = try AVAudioPlayer(contentsOf: url)
-            audioPlayer = sound
-            sound.play()
-        } catch {
-            // couldn't load file :(
-            print("error l")
-        }
+        playSound(soundFile: "d2")
     }
     
     @IBAction func d3(_ sender: Any) {
-        let path = Bundle.main.path(forResource: "d3", ofType:"mp3")!
-        let url = URL(fileURLWithPath: path)
-        
-        do {
-            let sound = try AVAudioPlayer(contentsOf: url)
-            audioPlayer = sound
-            sound.play()
-        } catch {
-            // couldn't load file :(
-            print("error l")
-        }
+        playSound(soundFile: "d3")
     }
     
     @IBAction func d4(_ sender: Any) {
-        let path = Bundle.main.path(forResource: "d4", ofType:"mp3")!
-        let url = URL(fileURLWithPath: path)
-        
-        do {
-            let sound = try AVAudioPlayer(contentsOf: url)
-            audioPlayer = sound
-            sound.play()
-        } catch {
-            // couldn't load file :(
-            print("error l")
-        }
+        playSound(soundFile: "d4")
     }
     
     @IBAction func d5(_ sender: Any) {
-        let path = Bundle.main.path(forResource: "d5", ofType:"mp3")!
-        let url = URL(fileURLWithPath: path)
-        
-        do {
-            let sound = try AVAudioPlayer(contentsOf: url)
-            audioPlayer = sound
-            sound.play()
-        } catch {
-            // couldn't load file :(
-            print("error l")
-        }
+        playSound(soundFile: "d5")
     }
     
     @IBAction func d6(_ sender: Any) {
-        let path = Bundle.main.path(forResource: "d6", ofType:"mp3")!
-        let url = URL(fileURLWithPath: path)
-        
-        do {
-            let sound = try AVAudioPlayer(contentsOf: url)
-            audioPlayer = sound
-            sound.play()
-        } catch {
-            // couldn't load file :(
-            print("error l")
-        }
+        playSound(soundFile: "d6")
     }
 
     @IBAction func d7(_ sender: Any) {
-        let path = Bundle.main.path(forResource: "d7", ofType:"mp3")!
-        let url = URL(fileURLWithPath: path)
-        
-        do {
-            let sound = try AVAudioPlayer(contentsOf: url)
-            audioPlayer = sound
-            sound.play()
-        } catch {
-            // couldn't load file :(
-            print("error l")
-        }
+        playSound(soundFile: "d7")
     }
     
-    @IBAction func d8(_ sender: Any) {
-        let path = Bundle.main.path(forResource: "Sequence 01", ofType:"mp3")!
-        let url = URL(fileURLWithPath: path)
-        
-        do {
-            let sound = try AVAudioPlayer(contentsOf: url)
-            audioPlayer = sound
-            sound.play()
-        } catch {
-            // couldn't load file :(
-            print("error l")
-        }
-    }
+
     
     
     
@@ -425,102 +149,39 @@ class ViewController: UIViewController,UIScrollViewDelegate  {
     
     
     @IBAction func l1(_ sender: Any) {
-        let path = Bundle.main.path(forResource: "l1", ofType:"mp3")!
-        let url = URL(fileURLWithPath: path)
-        
-        do {
-            let sound = try AVAudioPlayer(contentsOf: url)
-            audioPlayer = sound
-            sound.play()
-        } catch {
-            // couldn't load file :(
-            print("error l")
-        }
+        playSound(soundFile: "l1")
     }
     
     @IBAction func l2(_ sender: Any) {
-        let path = Bundle.main.path(forResource: "l2", ofType:"mp3")!
-        let url = URL(fileURLWithPath: path)
-        
-        do {
-            let sound = try AVAudioPlayer(contentsOf: url)
-            audioPlayer = sound
-            sound.play()
-        } catch {
-            // couldn't load file :(
-            print("error l")
-        }
+        playSound(soundFile: "l2")
     }
     @IBAction func l3(_ sender: Any) {
-        let path = Bundle.main.path(forResource: "l3", ofType:"mp3")!
-        let url = URL(fileURLWithPath: path)
-        
-        do {
-            let sound = try AVAudioPlayer(contentsOf: url)
-            audioPlayer = sound
-            sound.play()
-        } catch {
-            // couldn't load file :(
-            print("error l")
-        }
+        playSound(soundFile: "l3")
     }
     
     @IBAction func l4(_ sender: Any) {
-        let path = Bundle.main.path(forResource: "l4", ofType:"mp3")!
-        let url = URL(fileURLWithPath: path)
-        
-        do {
-            let sound = try AVAudioPlayer(contentsOf: url)
-            audioPlayer = sound
-            sound.play()
-        } catch {
-            // couldn't load file :(
-            print("error l")
-        }
+        playSound(soundFile: "l4")
     }
     
     @IBAction func l5(_ sender: Any) {
-        let path = Bundle.main.path(forResource: "l5", ofType:"mp3")!
-        let url = URL(fileURLWithPath: path)
-        
-        do {
-            let sound = try AVAudioPlayer(contentsOf: url)
-            audioPlayer = sound
-            sound.play()
-        } catch {
-            // couldn't load file :(
-            print("error l")
-        }
+        playSound(soundFile: "l5")
     }
     @IBAction func l6(_ sender: Any) {
-        let path = Bundle.main.path(forResource: "l6", ofType:"mp3")!
-        let url = URL(fileURLWithPath: path)
-        
-        do {
-            let sound = try AVAudioPlayer(contentsOf: url)
-            audioPlayer = sound
-            sound.play()
-        } catch {
-            // couldn't load file :(
-            print("error l")
-        }
+        playSound(soundFile: "l6")
     }
     @IBAction func l7(_ sender: Any) {
-        let path = Bundle.main.path(forResource: "l7", ofType:"mp3")!
-        let url = URL(fileURLWithPath: path)
-        
-        do {
-            let sound = try AVAudioPlayer(contentsOf: url)
-            audioPlayer = sound
-            sound.play()
-        } catch {
-            // couldn't load file :(
-            print("error l")
-        }
+        playSound(soundFile: "l7")
     }
     
     @IBAction func l8(_ sender: Any) {
-        let path = Bundle.main.path(forResource: "l8", ofType:"mp3")!
+        playSound(soundFile: "l8")
+    }
+    
+    
+    
+    func playSound(soundFile:String) {
+        
+        let path = Bundle.main.path(forResource: soundFile, ofType:"mp3")!
         let url = URL(fileURLWithPath: path)
         
         do {
@@ -534,6 +195,12 @@ class ViewController: UIViewController,UIScrollViewDelegate  {
     }
     
     
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
     
 }
 
